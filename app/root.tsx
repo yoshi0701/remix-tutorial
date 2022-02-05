0d40e5555370bd134db6f6abcb87033170b90cd4
tomoaki yoshioka <tomoaki.yoshiki@gmail.com>
2022-02-05 22:23:54 +0900

    feat: joker basic component added

--- app/root.tsx
+++ app/root.tsx
@@ -1,32 +1,19 @@
-import {
-  Links,
-  LiveReload,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration
-} from "remix";
-import type { MetaFunction } from "remix";
-
-export const meta: MetaFunction = () => {
-  return { title: "New Remix App" };
-};
+import { Links, LiveReload, Outlet } from "remix";
 
 export default function App() {
   return (
     <html lang="en">
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width,initial-scale=1" />
-        <Meta />
-        <Links />
-      </head>
-      <body>
-        <Outlet />
-        <ScrollRestoration />
-        <Scripts />
-        {process.env.NODE_ENV === "development" && <LiveReload />}
-      </body>
+    <head>
+      <meta charSet="utf-8" />
+      <title>Remix: So great, it's funny!</title>
+      <Links />
+    </head>
+    <body>
+    <Outlet />
+    {process.env.NODE_ENV === "development" ? (
+      <LiveReload />
+    ) : null}
+    </body>
     </html>
   );
 }
